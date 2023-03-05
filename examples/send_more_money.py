a36e586eec42ef45d6b6b5ade92cbfe20c6533f9
pjr <pjr@itee.uq.edu.au>
2023-03-05 11:57:42 +1000

    add 0 to choices in send_more_money description

--- examples/send_more_money.py
+++ examples/send_more_money.py
@@ -5,7 +5,7 @@ import pl_search as pls
 # but is given to illustrate the approach.
 
 # The puzzle is to choose distinct values for D,E,N,R,S,M,O,Y from
-# 1,2,3,4,5,6,7,8,9 to make the following addition true.
+# 0,1,2,3,4,5,6,7,8,9 to make the following addition true.
 
 #   SEND
 # + MORE
